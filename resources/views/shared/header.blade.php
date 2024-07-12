07139438dd1312936cf647da69102e5a4ed56252
salvatorebriga <salvo.z2302@gmail.com>
2024-07-13 00:09:10 +0200

    fix

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,46 +1,94 @@
 <header>
   <div class="container">
     <nav class="navbar">
-      <div class="container-fluid">
 
-        {{-- logo --}}
-        <img class="navbar-brand" src="https://upload.wikimedia.org/wikipedia/commons/3/3d/DC_Comics_logo.svg"
-          alt="Logo DC Comics"></img>
+      {{-- logo --}}
+      <img class="navbar-brand" src="https://upload.wikimedia.org/wikipedia/commons/3/3d/DC_Comics_logo.svg"
+        alt="Logo DC Comics">
 
-        {{-- navbar --}}
-        <ul class="navbar-nav bold d-flex flex-row gap-3">
-          <li class="nav-item">
-            <a class="nav-link" href="#">CHARACTERS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">COMICS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">MOVIES</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">TV</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">GAMES</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">COLLECTIBLES</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">VIDEOS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">FANS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">NEWS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">SHOP</a>
-          </li>
-        </ul>
+      {{-- hamburger --}}
+
+      <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel"></h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link" href="#">CHARACTERS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">COMICS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">MOVIES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">TV</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">GAMES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">COLLECTIBLES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">VIDEOS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">FANS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">NEWS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">SHOP</a>
+            </li>
+          </ul>
+        </div>
       </div>
+
+      {{-- navbar --}}
+
+      <ul class="navbar-nav d-none d-lg-flex flex-row gap-3 fw-bold">
+        <li>
+          <a class="nav-link py-3" href="#">CHARACTERS</a>
+        </li>
+        <li>
+          <a class="nav-link py-3" href="#">COMICS</a>
+        </li>
+        <li>
+          <a class="nav-link py-3" href="#">MOVIES</a>
+        </li>
+        <li>
+          <a class="nav-link py-3" href="#">TV</a>
+        </li>
+        <li>
+          <a class="nav-link py-3" href="#">GAMES</a>
+        </li>
+        <li>
+          <a class="nav-link py-3" href="#">COLLECTIBLES</a>
+        </li>
+        <li>
+          <a class="nav-link py-3" href="#">VIDEOS</a>
+        </li>
+        <li>
+          <a class="nav-link py-3" href="#">FANS</a>
+        </li>
+        <li>
+          <a class="nav-link py-3" href="#">NEWS</a>
+        </li>
+        <li>
+          <a class="nav-link py-3" href="#">SHOP</a>
+        </li>
+      </ul>
+
     </nav>
-  </div>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,3 @@
 @extends('layouts.app')
 @section('main')
-  SONO L'HOME
-  <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template">
 @endsection
