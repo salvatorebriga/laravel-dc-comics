480292141b079f703ca0ee25a837bef7b2066e12
salvatorebriga <salvo.z2302@gmail.com>
2024-07-12 17:54:13 +0200

    fix data type

--- database/migrations/2024_07_12_144351_create_comics_table.php
+++ database/migrations/2024_07_12_144351_create_comics_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
 
-            $table->string('title', 50);
+            $table->string('title');
             $table->text('description');
-            $table->string('thumb');
-            $table->decimal('price', 8, 2);
+            $table->text('thumb');
+            $table->string('price', 15);
             $table->string('series');
             $table->date('sale_date');
             $table->string('type');
