d3d2ae150d7e430f76ef921e97315f3dd84db213
salvatorebriga <salvo.z2302@gmail.com>
2024-07-12 22:54:41 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+
+    $data = [
+        'title' => 'Home DC',
+    ];
+
+    return view('welcome', $data);
+})->name('Home DC');
