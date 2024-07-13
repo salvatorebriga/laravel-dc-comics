8706d817505f3a4b48acb00305249e5945bf9850
salvatorebriga <salvo.z2302@gmail.com>
2024-07-13 03:32:40 +0200

    added footer botttom

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,4 +1,5 @@
 <footer>
   @include('shared.footer_top')
   @include('shared.footer_middle')
+  @include('shared.footer_bottom')
 </footer>
---
+++ resources/views/shared/footer_bottom.blade.php
@@ -0,0 +1,19 @@
+<section id="bottom">
+  <div class="container">
+    <div class="row row-cols-1 row-cols-sm-2">
+      <div class="col-left">
+        <button>
+          SING-UP NOW!
+        </button>
+      </div>
+      <div class="col-right">
+        <span>FOLLOW US</span>
+        <i class="fab fa-facebook"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-youtube"></i>
+        <i class="fab fa-pinterest"></i>
+        <i class="fab fa-instagram"></i>
+      </div>
+    </div>
+  </div>
+</section>
